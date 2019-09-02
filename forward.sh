d2ea9b0fb8cbd9df1bb80418b3794a5959468472
root <root@localhost.localdomain>
2019-09-02 02:13:18 -0400

    Add some scripts

---
+++ forward.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for name in $(cat list.txt);do
+	host $name.megacorpone.com|grep "has address" | cut -d" " -f1,4
+done
+
---
+++ ping-loop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for ip in $(seq 200 254); do
+	ping -c 1 10.11.1.$ip | grep "bytes from"| cut -d" " -f4|cut -d":" -f1 &
+done
